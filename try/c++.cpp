6ba9cf93559737512b266e87978de8407ae0e31c
yoyozi <2643198408@qq.com>
2024-11-11 16:19:34 +0800

    Add try directory with relevant files

---
+++ try/c++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include <iomanip> 
+using namespace std;
+template<class T>
+void (){
+	
+}
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+					
+	}
+	
+
+	return 0;
+}
